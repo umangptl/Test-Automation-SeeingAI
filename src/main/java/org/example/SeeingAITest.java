ba35c1f635b9487645aa8d79d601d4c1a74cd02a
Umang Patel <umang26patel@gmail.com>
2024-05-12 01:34:37 -0700

    extent-reporting
    Add extent test report frame work

--- src/main/java/org/example/SeeingAITest.java
+++ src/main/java/org/example/SeeingAITest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
@@ -14,7 +16,16 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -24,33 +35,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SeeingAITest {
+    private static ExtentReports extent;
+    private static ExtentTest test;
     public static AppiumDriver driver;
     public static int totalTests = 25;
     public static int passCount = 0;
 
+    @BeforeMethod
+    public void setUp() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("Spark.html");
+        extent = new ExtentReports();
+
+        extent.attachReporter(spark);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (extent != null) {
+            extent.flush();
+        }
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @AfterClass
+    public static void finalCheck() {
+        File reportFile = new File("Spark.html");
+        System.out.println("Final report generated: " + reportFile.exists());
+    }
+
     public static void main(String[] args) {
+        SeeingAITest testClass = new SeeingAITest();
+        testClass.setUp(); // Call setUp to initialize extent
+        test = extent.createTest("SeeingAI Test", "Verify SeeingAI functionality");
+
         appiumTest();
         double passRate = (double) passCount / totalTests * 100;
         System.out.println("Test Pass: " + passCount + "/" + totalTests );
         System.out.println("Pass %: " + passRate + "%");
+        testClass.tearDown();
     }
+
 
     public static void appiumTest() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "android");
         caps.setCapability("platformVersion", "13");
         caps.setCapability("deviceName", "pixel-8");
-        caps.setCapability("app", "package/.apk");
+        caps.setCapability("app", "seeingai.apk");
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("appPackage", "com.microsoft.seeingai");
-        caps.setCapability("appActivity", ".SplashActivity");
+        caps.setCapability("appActivity", "crc64a8457ff90b487ee0.SplashActivity");
 
-        List<String> expectedResults = readExpectedResultsFromFile();
+        List<String> expectedResults = readExpectedResultsFromFile("expected.txt");
 
         try {
             URL url = new URL("http://127.0.0.1:4723/wd/hub/");
             driver = new AndroidDriver(url, caps);
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
             skipButton(wait);
             acceptTerms(wait);
@@ -61,12 +104,7 @@ public class SeeingAITest {
             selectPhoto(wait);
 
             for (int i = 0; i < totalTests; i++) {
-                sharePhoto(wait);
-                String expectedResult = expectedResults.get(i);
-                getResults(wait, expectedResult);
-                compareResults(expectedResult);
-                goBack(wait);
-                swipeToNextImage();
+                appium(wait, expectedResults.get(i));
             }
 
         } catch (MalformedURLException | InterruptedException e) {
@@ -74,10 +112,18 @@ public class SeeingAITest {
         }
     }
 
-    private static List<String> readExpectedResultsFromFile() {
+    @Test
+    private static void appium(WebDriverWait wait, String expectedResult) throws InterruptedException {
+        sharePhoto(wait);
+        compareResults(wait, expectedResult);
+        goBack(wait);
+        swipeToNextImage();
+    }
+
+    private static List<String> readExpectedResultsFromFile(String filePath) {
         List<String> expectedResults = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("expected.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line = reader.readLine()) != null) {
                 expectedResults.add(line.trim());
@@ -89,16 +135,11 @@ public class SeeingAITest {
         return expectedResults;
     }
 
-    private static void getResults(WebDriverWait wait, String expectedResult) {
+    private static void compareResults(WebDriverWait wait, String expectedResult) {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.microsoft.seeingai:id/result_cell_text")));
-        String actualResult = element.getText();
+        String actualResult = element.getText().toLowerCase();
         System.out.println("Expected Result: " + expectedResult);
         System.out.println("Actual Result: " + actualResult);
-    }
-
-    private static void compareResults(String expectedResult) {
-        WebElement element = driver.findElement(By.id("com.microsoft.seeingai:id/result_cell_text"));
-        String actualResult = element.getText().toLowerCase();
         boolean pass = true;
         String[] expectedWords = expectedResult.toLowerCase().split(" ");
         for (String word : expectedWords) {
@@ -109,8 +150,10 @@ public class SeeingAITest {
         }
         if (pass) {
             passCount++;
+            test.log(Status.PASS, "Expected Result: " + expectedResult + ", Actual Result: " + actualResult);
             System.out.println("Pass");
         } else {
+            test.log(Status.FAIL, "Expected Result: " + expectedResult + ", Actual Result: " + actualResult);
             System.out.println("Fail");
         }
     }
@@ -118,36 +161,43 @@ public class SeeingAITest {
     private static void skipButton(WebDriverWait wait) {
         WebElement skipButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.microsoft.seeingai:id/pagedSkipButton")));
         skipButton.click();
+        test.log(Status.INFO, "Skip Tutorial");
     }
 
     private static void acceptTerms(WebDriverWait wait) {
         WebElement termsCheckBox = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.microsoft.seeingai:id/terms_check_box")));
         termsCheckBox.click();
+        test.log(Status.INFO, "Accept Terms and Conditions");
     }
 
     private static void getStarted(WebDriverWait wait) {
         WebElement getStartedButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.microsoft.seeingai:id/terms_getstarted_button")));
         getStartedButton.click();
+        test.log(Status.INFO, "Get Started");
     }
 
     private static void handlePermissionDialog(WebDriverWait wait) {
         WebElement allowButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")));
         allowButton.click();
+        test.log(Status.INFO, "Allow permission for photos");
     }
 
     private static void closeOverlay(WebDriverWait wait) {
         WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("com.microsoft.seeingai:id/close_icon_bottom_sheet")));
         closeButton.click();
+        test.log(Status.INFO, "Close Overlay");
     }
 
     private static void navigateToHome() {
         ((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.HOME));
+        test.log(Status.INFO, "Navigate to Home Screen");
         System.out.println("At Home");
     }
 
     private static void selectPhoto(WebDriverWait wait) {
         WebElement fileApp = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@content-desc=\"Photos\"]")));
         fileApp.click();
+        test.log(Status.INFO, "In Photos App");
         System.out.println("In Photos App");
 
         WebElement morePhotos = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.support.v7.widget.RecyclerView[@resource-id=\"com.google.android.apps.photos:id/recycler_view\"]/android.widget.FrameLayout/android.widget.LinearLayout")));
@@ -170,12 +220,12 @@ public class SeeingAITest {
         backButton.click();
     }
 
-
     private static void swipeToNextImage() throws InterruptedException {
         Thread.sleep(1000);
         TouchAction ta = new TouchAction((PerformsTouchActions) driver);
         ta.press(PointOption.point(900, 1200)).waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000))).moveTo(PointOption.point(100, 1200)).release().perform();
         Thread.sleep(1000);
+        test.log(Status.INFO, "Swipe to Next Image");
         System.out.println("swiped to next Image-----------------");
     }
 }
